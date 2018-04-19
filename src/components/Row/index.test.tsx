21bb8b501a80b5dfe233fc2954dec203831ead02
Michał Tęczyński <michalv8@gmail.com>
2018-04-19 21:58:20 +0200

    fix(test): Fixed invalid tests

--- src/components/Row/index.test.tsx
+++ src/components/Row/index.test.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import * as renderer from 'react-test-renderer'
-import { shallow } from 'enzyme'
 import Row from '.'
 
 describe('@components/Row', () => {
@@ -11,24 +10,13 @@ describe('@components/Row', () => {
         
         expect(tree).toMatchSnapshot()
     })
-
-    it('should render without errors', () => {
-        expect(
-            shallow(
-                <Row>Test</Row>
-            ).matchesElement(
-                <div className="row justify-content-md-center pl-3 pr-3">Test</div>
-            )
-        ).toBeTruthy()
-    })
     
     it('should render without errors, props (withTopPadding = true)', () => {
-        expect(
-            shallow(
-                <Row withTopPadding>Test</Row>
-            ).matchesElement(
-                <div className="row justify-content-md-center pl-3 pr-3 pt-4">Test</div>
-            )
-        ).toBeTruthy()
+
+        const tree = renderer.create(
+            <Row withTopPadding>Test</Row>
+        ).toJSON()
+        
+        expect(tree).toMatchSnapshot()
     })
 })
