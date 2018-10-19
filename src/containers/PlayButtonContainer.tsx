1ebf53ff1648c6ac35ade628142c11717a91f178
michalv8 <michal.teczynski@gmail.com>
2018-10-19 19:16:40 +0200

    Removed SoundPlayerContainer

--- src/containers/PlayButtonContainer.tsx
+++ src/containers/PlayButtonContainer.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react'
 import Sound from '../types/Sound'
-import SoundCallback from '../types/SoundCallback'
 import PlayButton from '../components/PlayButton'
-import AddSoundStateListenerCallback from '../types/AddSoundStateListenerCallback'
 
 export interface Props {
     sound: Sound
-    playAudio: SoundCallback<void>,
-    stopAudio: SoundCallback<void>,
     isPlaying?: boolean,
-    addSoundStateListener: AddSoundStateListenerCallback
 }
 
 export interface State {
@@ -21,8 +16,6 @@ export default class PlayButtonContainer extends React.PureComponent<Props, Stat
     static defaultProps: Partial<Props> = {
         isPlaying: false,
     }
-
-    private mounted: boolean = false
 
     constructor(props: Props) {
         super(props)
@@ -36,52 +29,18 @@ export default class PlayButtonContainer extends React.PureComponent<Props, Stat
         this.isPlaying = this.isPlaying.bind(this)
     }
 
-    componentWillUnmount() {
-        this.mounted = false
-    }
-
-    componentDidMount() {
-        this.mounted = true
-        this.props.addSoundStateListener(
-            this.props.sound,
-            (sound: Sound, isPlaying: boolean): void => {
-                if (!this.mounted) {
-                    return
-                }
-                
-                this.setState({
-                    isPlaying,
-                    isLoading: false,
-                })
-            })
-    }
-
     isPlaying(): boolean {
         return this.state.isPlaying
     }
 
     onClick() {
-        const {
-            playAudio,
-            stopAudio,
-            sound,
-        } = this.props
-
         if (this.state.isLoading) {
             return
         }
 
         this.setState({
             isLoading: true,
-        })
-
-        if (this.isPlaying()) {
-            stopAudio(sound)
-
-            return
-        }
-
-        playAudio(sound)       
+        })     
     }
 
     render() {
--- src/routes/Sounds/index.tsx
+++ src/routes/Sounds/index.tsx
@@ -3,49 +3,39 @@ import Container from '../../components/Container'
 import Row from '../../components/Row'
 import SoundsNav from '../../components/SoundsNav'
 import SoundsContainer from '../../containers/SoundsContainer'
-import SoundsPlayerContainer from '../../containers/SoundsPlayerContainer'
 import SoundsRow from '../../components/SoundsRow'
 import Tag from '../../types/Tag'
 import TabsContainer from '../../containers/TabsContainer'
 import SoundsRepository from '../../lib/sounds-repository'
 import TagsRepository from '../../lib/tags-repository'
-import SoundPlayer from '../../types/SoundPlayer'
 import DefaultTemplate from '../../templates/DefaultTemplate'
 
 export default function Sounds(): React.ReactElement<{}> {
   return (
     <SoundsContainer>
       {(soundsRepository: SoundsRepository, tagsRepository: TagsRepository) => (
-        <SoundsPlayerContainer sounds={soundsRepository.getSounds()}>
-          {(soundPlayer: SoundPlayer) => (
-            <TabsContainer initialTab={tagsRepository.getTagsByOrder()[1]}>
-              {(currentTag: Tag | null, changeTab: {(tab: any): void}) => (
-                <DefaultTemplate>
-                  <Container fluid>
-                    <Row>
-                      <SoundsNav
-                        currentTag={currentTag}
-                        tags={tagsRepository
-                          .getTagsByOrder()
-                          .filter((tag: Tag) => 
-                            tag.sounds || ['all', 'misc', 'recent'].includes(tag.slug)
-                          )}
-                        changeTag={changeTab}
-                      />
-                    </Row>
-                    <SoundsRow
-                      sounds={soundsRepository.getSoundsForTag(currentTag)}
-                      playAudio={soundPlayer.playAudio}
-                      stopAudio={soundPlayer.stopAudio}
-                      isPlaying={soundPlayer.isPlaying}
-                      addSoundStateListener={soundPlayer.addSoundStateListener}
-                    />
-                  </Container>
-                </DefaultTemplate>
-              )}
-            </TabsContainer>
+        <TabsContainer initialTab={tagsRepository.getTagsByOrder()[1]}>
+          {(currentTag: Tag | null, changeTab: {(tab: any): void}) => (
+            <DefaultTemplate>
+              <Container fluid>
+                <Row>
+                  <SoundsNav
+                    currentTag={currentTag}
+                    tags={tagsRepository
+                      .getTagsByOrder()
+                      .filter((tag: Tag) => 
+                        tag.sounds || ['all', 'misc', 'recent'].includes(tag.slug)
+                      )}
+                    changeTag={changeTab}
+                  />
+                </Row>
+                <SoundsRow
+                  sounds={soundsRepository.getSoundsForTag(currentTag)}
+                />
+              </Container>
+            </DefaultTemplate>
           )}
-        </SoundsPlayerContainer>
+        </TabsContainer>
       )}
     </SoundsContainer>
   )
