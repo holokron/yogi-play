175f3d9e33f70dfd28e21f90e2d6dd784c88559d
Michał Tęczyński <michalv8@gmail.com>
2019-04-23 23:02:24 +0200

    Minor improvements and added actions tests

--- src/components/PlayButton/index.tsx
+++ src/components/PlayButton/index.tsx
@@ -1,101 +1,87 @@
-import * as React from 'react'
-import Button from 'reactstrap/lib/Button'
-import ButtonGroup from 'reactstrap/lib/ButtonGroup'
-import './index.css'
-import Sound from '../../types/Sound'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as React from "react";
+import Button from "reactstrap/lib/Button";
+import ButtonGroup from "reactstrap/lib/ButtonGroup";
+import "./index.css";
+import Sound from "../../types/Sound";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export interface Props {
-    sound: Sound
-    onClick?: () => void
-    onFavClick?: () => void
-    isPlaying?: boolean
-    isLoading?: boolean
-    isFavourite?: boolean
+  sound: Sound;
+  onClick?: () => void;
+  onFavClick?: () => void;
+  isPlaying?: boolean;
+  isLoading?: boolean;
+  isFavourite?: boolean;
 }
 
-export default class PlayButton extends React.PureComponent<Props, {}> {
-    public static defaultProps: Partial<Props> = {
-        isPlaying: false,
-        isLoading: false,
-        isFavourite: false,
+export default class PlayButton extends React.PureComponent<Props> {
+  public static defaultProps: Partial<Props> = {
+    isPlaying: false,
+    isLoading: false,
+    isFavourite: false
+  };
+
+  public renderPlayButton() {
+    const { sound } = this.props;
+
+    return <Button {...this.getPlayButtonProps()}>{sound.name}</Button>;
+  }
+
+  public renderFavouriteButton() {
+    return (
+      <Button {...this.getFavButtonProps()}>
+        <FontAwesomeIcon icon="star" />
+      </Button>
+    );
+  }
+
+  public render() {
+    return (
+      <ButtonGroup {...this.getButtonGroupProps()}>
+        {this.renderPlayButton()}
+        {this.renderFavouriteButton()}
+      </ButtonGroup>
+    );
+  }
+
+  private getButtonGroupProps(): object {
+    const { isLoading, isPlaying } = this.props;
+
+    let animationClass: string = "";
+
+    if (isLoading) {
+      animationClass += "animated infinite flash";
     }
 
-    public renderPlayButton() {
-        const {
-            sound,
-        } = this.props
-
-        return (
-            <Button {...this.getPlayButtonProps()}>
-                {sound.name}
-            </Button>
-        )
+    if (isPlaying) {
+      animationClass += "animated infinite pulse active";
     }
 
-    public renderFavouriteButton() {
-        return (
-            <Button {...this.getFavButtonProps()}>
-                <FontAwesomeIcon icon="star" />
-            </Button>
-        )
-    }
+    return {
+      className: `play-btn d-flex ${animationClass}`,
+      size: "sm"
+    };
+  }
 
-    public render() {
-        return (
-            <ButtonGroup {...this.getButtonGroupProps()}>
-                {this.renderPlayButton()}
-                {this.renderFavouriteButton()}
-            </ButtonGroup>
-        )
-    }
+  private getPlayButtonProps(): object {
+    const { onClick } = this.props;
 
-    private getButtonGroupProps(): object {
-        const {
-            isLoading,
-            isPlaying,
-        } = this.props
+    return {
+      className: "play-btn__play text-truncate text-uppercase font-weight-bold",
+      color: "primary",
+      outline: true,
+      onClick
+    };
+  }
 
-        let animationClass: string = ''
+  private getFavButtonProps(): object {
+    const { isFavourite, onFavClick } = this.props;
 
-        if (isLoading) {
-            animationClass += 'animated infinite flash'
-        }
-
-        if (isPlaying) {
-            animationClass += 'animated infinite pulse active'
-        }
-
-        return {
-            className: `play-btn d-flex ${animationClass}`,
-            size: 'sm',
-        }
-    }
-
-    private getPlayButtonProps(): object {
-        const {
-            onClick,
-        } = this.props
-
-        return {
-            className: 'play-btn__play text-truncate text-uppercase font-weight-bold',
-            color: 'primary',
-            outline: true,
-            onClick,
-        }
-    }
-
-    private getFavButtonProps(): object {
-        const {
-            isFavourite,
-            onFavClick,
-        } = this.props
-
-        return {
-            className: 'play-btn__fav',
-            color: 'primary',
-            outline: !isFavourite,
-            onClick: onFavClick,
-        }
-    }
+    return {
+      className: "play-btn__fav",
+      color: "primary",
+      outline: !isFavourite,
+      onClick: onFavClick
+    };
+  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'animate.css/animate.min.css'
-import './icons'
-import 'react-app-polyfill/ie11'
-import 'core-js-pure/features/object/keys'
-import 'core-js-pure/features/object/values'
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import './icons'
-import App from './App'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "animate.css/animate.min.css";
+import "./icons";
+import "react-app-polyfill/ie11";
+import "core-js-pure/features/object/keys";
+import "core-js-pure/features/object/values";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./icons";
+import App, { APP_VERSION } from "./App";
 
 if ("production" === process.env.NODE_ENV && process.env.REACT_APP_SENTRY_DSN) {
   require("@sentry/browser").init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
-    release: "0.31.0",
-  })
+    release: APP_VERSION
+  });
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-)
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
