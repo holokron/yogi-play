e28f51d49d920537b4b686b987f816bea8232e65
Michał Tęczyński <michalv8@gmail.com>
2019-04-23 23:01:57 +0200

    Removed ToggleContainer and updated Navigation

--- src/components/Navigation/index.test.tsx
+++ src/components/Navigation/index.test.tsx
@@ -1,39 +1,22 @@
-import * as React from 'react'
-import * as renderer from 'react-test-renderer'
-import { mount } from 'enzyme'
-import { MemoryRouter } from 'react-router-dom'
-import Navigation from '.'
-import configureStore from '../../store'
-import { Provider } from 'react-redux'
+import * as React from "react";
+import * as renderer from "react-test-renderer";
+import { MemoryRouter } from "react-router-dom";
+import Navigation from ".";
+import configureStore from "../../store";
+import { Provider } from "react-redux";
 
-const store = configureStore()
+const store = configureStore();
 
-describe('@components/Navigation', () => {
-    it('renders correctly', () => {
-        const tree = renderer.create(
-            <Provider store={store}>
-                <MemoryRouter>
-                    <Navigation />
-                </MemoryRouter>
-            </Provider>
-        ).toJSON()
-        
-        expect(tree).toMatchSnapshot()
-    })
+describe("@components/Navigation", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Navigation />
+        </MemoryRouter>
+      </Provider>
+    );
 
-    it('should toggle menu', () => {
-        const result = mount(
-            <Provider store={store}>
-                <MemoryRouter>
-                    <Navigation />
-                </MemoryRouter>
-            </Provider>
-        )
-
-        expect(result.find('div.navbar-collapse.collapse.show')).toHaveLength(0)
-
-        result.find('button.navbar-toggler').simulate('click')
-
-        expect(result.find('div.navbar-collapse.collapsing')).toHaveLength(1)
-    })    
-})
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
