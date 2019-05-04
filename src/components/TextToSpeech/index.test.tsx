a34c917ea275937e4b51fee74c6fa01049943b3f
Michał Tęczyński <michalv8@gmail.com>
2019-05-04 20:16:33 +0200

    Changed TextToSpeech component

---
+++ src/components/TextToSpeech/index.test.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import * as renderer from "react-test-renderer";
+import TextToSpeech, { TextToSpeechProps } from ".";
+
+describe("@components/TextToSpeech", () => {
+  it.each([
+    [
+      {
+        readText: (): void => {}
+      }
+    ]
+  ] as TextToSpeechProps[][])(
+    "renders correctly with props: %p",
+    (props: TextToSpeechProps) => {
+      const tree = renderer.create(<TextToSpeech {...props} />).toJSON();
+
+      expect(tree).toMatchSnapshot();
+    }
+  );
+});
