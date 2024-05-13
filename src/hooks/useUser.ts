36e0692c00ea43f204d4369ec9d31d22e98a5f83
Michał Tęczyński <m.teczynski@text.com>
2024-05-13 19:54:59 +0200

    Removed unused files

--- src/hooks/useUser.ts
+++
@@ -1,7 +0,0 @@
-import User from "@/types/User";
-import { useSelector } from "react-redux";
-import { getUser } from "@/store/selectors";
-
-export default function useUser(): User | null {
-  return useSelector(getUser);
-}
--- src/types/SoundsWithTag.ts
+++
@@ -1,7 +0,0 @@
-import Sound from "./Sound";
-import Tag from "./Tag";
-
-export default interface SoundsWithTag {
-  tag: Tag;
-  sounds: Sound[];
-}
