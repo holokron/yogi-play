aae80411a4114e153024692c1b5a5ef57bf0f965
Michał Tęczyński <m.teczynski@text.com>
2024-05-20 20:01:27 +0200

    Updated header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,15 +3,17 @@ import { ModeToggle } from "@/components/ModeToggle";
 import { Sidebar } from "@/components/Sidebar";
 import { NavMenu } from "@/components/NavMenu";
 import { Link } from "react-router-dom";
+import { SoundSearchInput } from "@/components/SoundSearchInput";
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="bg-slate-300 dark:bg-background fixed w-full">
-      <header className="flex justify-between items-center border-b px-4 p-2 gap-2">
+      <header className="bg-slate-900 flex justify-between items-center border-b px-4 p-2 gap-2 text-background dark:text-foreground border-foreground dark:border-background">
         <Link to="/" className=" flex items-center gap-2">
           <img alt="Yogi PLAY" width={30} src="/icons/icon-64x64.png" /> Yogi
           PLAY
         </Link>
+        <SoundSearchInput />
         <div className="flex gap-2">
           <ModeToggle />
           <NavMenu />
--- src/components/SoundSearchInput.tsx
+++ src/components/SoundSearchInput.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import useSoundSearch from "@/hooks/use-sound-search";
 import { useSelector } from "react-redux";
 import { getSoundsFilter } from "@/store/selectors";
+import { Search } from "lucide-react";
 
 export const SoundSearchInput: FC = () => {
   const { onChange } = useSoundSearch();
@@ -13,12 +14,16 @@ export const SoundSearchInput: FC = () => {
   };
 
   return (
-    <Input
-      type="search"
-      placeholder="Szukaj dźwięku"
-      className="w-40 md:w-64"
-      value={soundsFilter ?? ""}
-      onChange={handleChange}
-    />
+    <div className="flex gap-2 items-center">
+      <Search size={24}/>
+      <Input
+        size={12}
+        type="search"
+        placeholder="Szukaj dźwięku... ⌘ + K"
+        className="w-40 md:w-64 h-8 text-foreground"
+        value={soundsFilter ?? ""}
+        onChange={handleChange}
+      />
+    </div>
   );
 };
